81ffd2499643a1adef4a5a350f764d1950bdbf10
ilmir <ilmir@DESKTOP-OLFAF11>
2024-04-01 20:38:26 +0300

    добавил отсчет секунд

--- main.py
+++ main.py
@@ -1,48 +1,27 @@
 import os
 import subprocess
 
+import asyncio
+import aiofiles
 
 
-args=["zip","-",'t.txt' ]
-procces=subprocess.Popen(args,stdin=subprocess.PIPE,stdout=subprocess.PIPE)
-a,b=procces.communicate()
-print(a,b)
-
-# print(data)
-# code=procces.wait()
-# args=[f'zip', 'archive.zip <<',]
-# subprocess.call(args,stdin=archive)
-
-# with open("t.zip","wb") as file:
-#     file.write(archive)
-# args=['zip','a.zip',file]
+async def cmd_run(command):
+    proc = await asyncio.create_subprocess_shell(
+        command,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+    while not proc.stdout.at_eof():
+        stdout = await proc.stdout.read(n=100)
+        print(stdout)
+        yield stdout
 
 
-# with open("t.zip","wb") as file:
-   
-#     args=['zip','a.zip',file]
-#     subprocess.call(args=args, shell=True)
-
-# import asyncio
-
-
-# async def cmd_run(command):
-#     proc = await asyncio.create_subprocess_shell(
-#         command,
-#         stdout=asyncio.subprocess.PIPE,
-#         stderr=asyncio.subprocess.PIPE)
-    
-#     stdout, stderr = await proc.communicate()
-#     print(stdout,stderr)
+async def main():
+    command = 'zip - t.txt '
+    async with aiofiles.open("archive.zip", "wb") as file:
+        async for content in cmd_run(command):
+            await file.write(content)
 
 
-
-# async def main():
-#     command='zip - t.txt '
-#     result=await cmd_run(command)
-#     print(result)
-
-
-
-# if __name__ =='__main__':
-#     asyncio.run(main())
+if __name__ == '__main__':
+    asyncio.run(main())
