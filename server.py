fa6ade81e264fe159a3a9fb2051de3896f89ea89
root <root@DESKTOP-6FVMPIO>
2024-07-24 15:31:02 +0300

    Добавил argparse

--- server.py
+++ server.py
@@ -2,8 +2,9 @@ import asyncio
 from aiohttp import web
 import aiofiles 
 import logging
+import argparse
 
-logging.basicConfig(level = logging.DEBUG)
+from functools import partial
 import os
 
 
@@ -24,10 +25,10 @@ async def handle_index_page(request):
     return web.Response(text=index_contents, content_type='text/html')
 
 
-async def get_archive(request):
+async def get_archive(request,delay,path):
     
     hash=request.match_info.get('archive_hash', "Anonymous")
-    path=f"photos/{hash}/"
+    path=f"{path}/{hash}/"
     
     if not os.path.exists(path):
         raise web.HTTPNotFound(text='Архив не существует или был удален')
@@ -37,6 +38,10 @@ async def get_archive(request):
     response.headers['Content-Disposition:']='attachment; filename=archive.zip'
     await response.prepare(request)
     process=await get_process(path)
+
+    if delay:
+        await asyncio.sleep(delay)
+
     try:
         while not process.stdout.at_eof():
             stdout = await process.stdout.read(n=100)
@@ -59,10 +64,22 @@ async def get_archive(request):
 
 
 if __name__ == '__main__':
+
+
+    parser = argparse.ArgumentParser(
+        description="Создает из ссылки битлинк или показывает кол-во переходов по битлинку")
+    parser.add_argument('-l', '--logging', type=bool,default='True')
+    parser.add_argument('-d', '--delay', type=int, default=None)
+    parser.add_argument('-p', '--path', type=str,default='photos')
+    args = parser.parse_args()
+
+    if args.logging:
+        logging.basicConfig(level=logging.INFO)
+
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
-        web.get('/archive/{archive_hash}/', get_archive),
+        web.get('/archive/{archive_hash}/', partial(get_archive,delay=args.delay,path=args.path)),
        
     ])
     web.run_app(app)
